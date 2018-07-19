2200210545cdc0d8a7138cef0303e1e3b4347fa0
alarichadef <alarichadef@gmail.com>
2018-07-19 08:14:18 +0200

    Beginning the build of the login page (From hello-ionic), Add all elements ask in the wireframe, waiting for css
    -Modification of the page hello-ionic
    -Add an id to menu and method to desactivate the menu in the login page
    (Have to activate on the other page)
    -All the element ask are on the page
    -Miss CSS and we will have the same wireframe

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(  public menuCtrl: MenuController) {
+    this.menuCtrl.swipeEnable(false);
 
   }
-}
+
+
+ }
